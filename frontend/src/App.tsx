74f2c93197d5115b0c1b033c05706eacc31928ff
brvndonkoh <e1121709@u.nus.edu>
2026-03-07 13:00:12 +0800

    feat: register Match into router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from '../src/pages/Login';
 import Signup from '../src/pages/Signup';
 import Index from '../src/pages/Index';
+import Match from './pages/Match';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Route path="/" element={<Index />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/match" element={<Match />} />
 
       {/* Fallback */}
       <Route path="*" element={<div>404 Not Found</div>} />
